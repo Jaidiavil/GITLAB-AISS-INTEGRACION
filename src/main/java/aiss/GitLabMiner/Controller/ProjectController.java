7981ef3ba209f44c2de854249e08d9265915e453
germanmoragaa <german.moraga.fontan@gmail.com>
2023-05-08 12:48:36 +0200

    findOne

--- src/main/java/aiss/GitLabMiner/Controller/ProjectController.java
+++ src/main/java/aiss/GitLabMiner/Controller/ProjectController.java
@@ -1,8 +1,17 @@
 package aiss.GitLabMiner.Controller;
 import aiss.GitLabMiner.model.Project;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import aiss.GitLabMiner.repository.ProjectRepository;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import javax.validation.Valid;
+
+
+
 
 import java.util.List;
 
@@ -19,5 +28,9 @@ public class ProjectController {
         return repository.findAll();
     }
 
-
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@PathVariable String id, @Valid @RequestBody Project updatedProject) {
+        repository.update(updatedProject, id);
+    }
 }

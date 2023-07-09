07bdc7cf59b409936e0298dc4932eec4e503ecef
aloporsan <aloporsan@alum.us.es>
2023-07-09 12:06:04 +0200

    Definitivo 101%

--- src/main/java/aiss/GitLabMiner/controller/GitLabController.java
+++ src/main/java/aiss/GitLabMiner/controller/GitLabController.java
@@ -14,17 +14,19 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/gitlab")
+@RequestMapping("/gitlabminer")
 public class GitLabController {
 
     @Autowired
     GitLabService gitLabService;
 
+    //POST http://localhost:8081/gitlabminer/
     @GetMapping
     public List<Project> findAll(){
         return gitLabService.getProjects();
     }
 
+    //GET http://localhost:8081/gitlabminer/{id}
     @GetMapping("/{id}")
     public Project findOne(@PathVariable String id,
                            @RequestParam(defaultValue = "2") Integer sinceCommits,
@@ -40,6 +42,7 @@ public class GitLabController {
         }
     }
 
+    //POST http://localhost:8081/gitlabminer/{id}
     @PostMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Project createProject(@PathVariable @Valid String id,
